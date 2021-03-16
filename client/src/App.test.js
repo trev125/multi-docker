a0bfb706bd038af72d3e8109f258de362046fde2
Trevor Allen <tallen@nav.com>
2021-03-15 21:45:07 -0600

    initial commit

---
+++ client/src/App.test.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { render, screen } from "@testing-library/react"
+import App from "./App"
+
+test("renders learn react link", () => {})
+
+// test("test one value", () => {
+// 	fireEvent.keyPress(getByTestId("fibInput"), { key: "3", code: "Digit3" })
+// 	fireEvent.click(getByTestId("fibInputBtn"))
+// })
